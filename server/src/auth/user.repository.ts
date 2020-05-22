8ce11f861b32eb3a434887816262a85df5951251
Nikolay Pavlov <nikolaypavlov4@gmail.com>
2020-05-22 14:27:29 +0300

    Fixes #4

--- server/src/auth/user.repository.ts
+++ server/src/auth/user.repository.ts
@@ -1,10 +1,20 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from '../database/entities/user.entity';
 import { LoginUserDTO } from '../models/dto/user/login-user.dto';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { UserDTO } from 'src/models/dto/user/user.dto';
+import { plainToClass } from 'class-transformer';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+
+  public async viewUserById(userId: string): Promise<UserDTO> {
+    const foundUser: User = await this.findOne(userId);
+        if (!foundUser) {
+        throw new NotFoundException(`User does not exist.`);
+        }
+        return plainToClass(UserDTO, foundUser, { excludeExtraneousValues: true });
+}
 
     public async matchEmail(email: string): Promise<void> {
       const matchEmail = await this.findOne({ 
