ebc5249164538784a40fa61943e821def4c9985c
Nikolay Pavlov <nikolaypavlov4@gmail.com>
2020-05-22 14:27:09 +0300

    Fixes #20

--- server/src/admin/employee.repository.ts
+++ server/src/admin/employee.repository.ts
@@ -1,6 +1,8 @@
 import { Employee } from '../database/entities/employee.entity';
 import { EntityRepository, Repository } from 'typeorm';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { EmployeeDTO } from 'src/models/dto/employee/employee.dto';
+import { plainToClass } from 'class-transformer';
 @EntityRepository(Employee)
 export class EmployeeRepository extends Repository<Employee> {
 
@@ -13,4 +15,11 @@ export class EmployeeRepository extends Repository<Employee> {
         }
     }
 
+    public async viewEmployeeById(employeeId: string): Promise<EmployeeDTO> {
+      const foundEmployee: Employee = await this.findOne(employeeId);
+          if (!foundEmployee) {
+          throw new NotFoundException(`Employee does not exist.`);
+          }
+          return plainToClass(EmployeeDTO, foundEmployee, { excludeExtraneousValues: true });
+  }
 }
