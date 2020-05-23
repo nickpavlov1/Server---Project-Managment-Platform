b3f86c1afa8fc6d21a88cfa0c43c3b8c7362dac5
Aleksandr  Stepanov <oum.aleksandr@gmail.com>
2020-05-23 23:28:09 +0300

    change db type to any

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,9 +7,9 @@ import { ConfigService, ConfigModule } from '@nestjs/config';
         imports: [ConfigModule],
         inject: [ConfigService],
         useFactory: async (configService: ConfigService) => ({
-          type: 'mysql' || configService.get('DB_TYPE'),
+          type: 'mysql' as any || configService.get('DB_TYPE'),
           host: 'localhost' || configService.get('DB_HOST'),
-          port: process.env.CLEARDB_DATABASE_URL || +configService.get('DB_PORT'),
+          port: +process.env.CLEARDB_DATABASE_URL || +configService.get('DB_PORT'),
           username: 'b065ce5d3e793e' || configService.get('DB_USERNAME'),
           password: '356e188e' || configService.get('DB_PASSWORD'),
           database: 'heroku_cb78149dbf0a417' || configService.get('DB_DATABASE_NAME'),
