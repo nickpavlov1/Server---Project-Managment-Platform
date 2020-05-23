922b1dc36d73f7d8e7d21644485f1c72f56827d5
Aleksandr  Stepanov <oum.aleksandr@gmail.com>
2020-05-23 23:59:00 +0300

    change port and url

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,8 +15,8 @@ import * as Joi from '@hapi/joi';
       validationSchema: Joi.object({
         PORT: Joi.number().default(3000),
         DB_TYPE: Joi.string().default('mysql'),
-        DB_HOST: Joi.string().default(process.env.IP),
-        DB_PORT: Joi.number().default(+process.env.CLEARDB_DATABASE_URL),
+        DB_HOST: Joi.string().default(process.env.CLEARDB_DATABASE_URL),
+        DB_PORT: Joi.number().default(3306),
         DB_USERNAME: Joi.string().default('b065ce5d3e793e'),
         DB_PASSWORD: Joi.string().default('356e188e'),
         DB_DATABASE_NAME: Joi.string().default('heroku_cb78149dbf0a417'),
